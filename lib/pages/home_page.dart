7cdb942343b7d25c8a86d5c328f6e1c4acf28e29
ahmedelne7 <ahmedelnahrawy881@gmail.com>
2024-11-19 15:36:01 +0200

    adding product card

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -9,24 +9,75 @@ class HomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        actions: [
-          IconButton(
-              onPressed: () {},
-              icon: Icon(
-                FontAwesomeIcons.cartPlus,
-                color: Colors.black,
-              ))
-        ],
-        centerTitle: true,
-        title: Text(
-          'New Trend',
-          style: TextStyle(color: Colors.black),
+        appBar: AppBar(
+          actions: [
+            IconButton(
+                onPressed: () {},
+                icon: Icon(
+                  FontAwesomeIcons.cartPlus,
+                  color: Colors.black,
+                ))
+          ],
+          centerTitle: true,
+          title: Text(
+            'New Trend',
+            style: TextStyle(color: Colors.black),
+          ),
+          elevation: 0,
+          backgroundColor: Colors.white,
+          leading: Icon(Icons.arrow_back_ios_rounded),
         ),
-        elevation: 0,
-        backgroundColor: Colors.white,
-        leading: Icon(Icons.arrow_back_ios_rounded),
-      ),
-    );
+        body: Container(
+          height: 130,
+          width: 200,
+          decoration: BoxDecoration(
+            boxShadow: [
+              BoxShadow(
+                blurRadius: 50,
+                color: Colors.grey.withOpacity(0.2),
+                spreadRadius: 0,
+                offset: Offset(10, 10)
+              )
+            ]
+          ),
+          child: Card(
+            elevation: 10,
+            child: Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 16, vertical: 16),
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.end,
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text(
+                    'Product Name',
+                    style: TextStyle(
+                      color: Colors.grey,
+                      fontSize: 16,
+                    ),
+                  ),
+                  SizedBox(
+                    height: 10,
+                  ),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      Text(
+                        r'$250',
+                        style: TextStyle(
+                          color: Colors.grey,
+                          fontSize: 16,
+                        ),
+                      ),
+                      Icon(
+                        Icons.favorite_rounded,
+                        color: Colors.red,
+                      )
+                    ],
+                  )
+                ],
+              ),
+            ),
+          ),
+        ));
   }
 }
